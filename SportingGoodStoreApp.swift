39b58538a98085433399fa3a633b3c1cbcebcd47
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-07 18:30:43 +0100

    Initial commit

---
+++ SportingGoodStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportingGoodStoreApp.swift
+//  SportingGoodStore
+//
+//  Created by Nicola Kaleta on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SportingGoodStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
